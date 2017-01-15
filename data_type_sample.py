a9ba75a106d188c866636ac816526c604fe2c8f3
takmnagaya <t-nagaya@towatech.net>
2017-01-15 18:29:02 +0900

    Python データ型出力サンプル
    $ python data_type_sample.py
    <class 'int'>
    <class 'float'>
    <class 'str'>

---
+++ data_type_sample.py
@@ -0,0 +1,15 @@
+
+# 整数型
+integer_type = type(10)
+print(integer_type)
+# >> <class 'int'>
+
+# 小数型
+float_type = type(10.2)
+print(float_type)
+# >> <class 'float'>
+
+# 文字列型
+string_type = type("hoge")
+print(string_type)
+# >> <class 'str'>
