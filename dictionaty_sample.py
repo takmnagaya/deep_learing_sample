94d4eadf78849344568e84c46a976dfb9c8ca19f
takmnagaya <taknagaya@gmail.com>
2017-01-15 18:50:29 +0900

    Python ディクショナリサンプル
    $ python dictionaty_sample.py
    {'height': 175}
    175
    {'height': 175, 'weight': 65}

---
+++ dictionaty_sample.py
@@ -0,0 +1,11 @@
+me = {'height': 175}
+print(me)
+# >> {'height': 175} # ディクショナリ出力
+
+print(me['height'])
+# >> 175 # ディクショナリにアクセス
+
+me['weight'] = 65
+print(me)
+# >> {'height': 175, weight: 65} # ディクショナリに新しい要素を追加
+
